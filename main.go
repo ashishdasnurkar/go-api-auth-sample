2e962aa2840dbf1daf73118dc1aa478164791a7b
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2023-11-04 20:38:05 +1100

    wip signup

--- main.go
+++ main.go
@@ -10,9 +10,14 @@ import (
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/signup", signup).Methods("POST")
 
 	fmt.Println("Server started running on http://localhost:8080")
 	http.ListenAndServe(":8080", r)
+}
+
+func signup(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Singup invoked")
 }
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
